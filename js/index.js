7c978d8a712f2d6227159776233ce0da0ee9e2b0
AndyGuit <totalslash@msn.com>
2021-10-18 19:18:27 +0300

    init: start project

---
+++ js/index.js
@@ -0,0 +1,293 @@
+import playList from './playList.js';
+
+const timeBlock = document.querySelector('time.time');
+const dateBlock = document.querySelector('date.date');
+const greetingBlock = document.querySelector('span.greeting');
+const nameBlock = document.querySelector('input.name');
+const body = document.querySelector('body');
+const slideNext = document.querySelector('button.slide-next');
+const slidePrev = document.querySelector('button.slide-prev');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const cityInput = document.querySelector('input.city');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const quote = document.querySelector('.quote');
+const quoteAuthor = document.querySelector('.author');
+const quoteBtn = document.querySelector('button.change-quote');
+const audio = new Audio();
+const playBtn = document.querySelector('button.play');
+const nextBtn = document.querySelector('button.play-next');
+const prevBtn = document.querySelector('button.play-prev');
+const playListContainer = document.querySelector('ul.play-list');
+const audioProgress = document.querySelector('.progress>input');
+const audioVol = document.querySelector('.volume>input');
+const volBtn = document.querySelector('.volume button');
+
+let randomNum = Math.floor(Math.random() * 20) + 1;
+let playNum = 0;
+let isPlay = false;
+let isMouseDown = false;
+
+
+function showTime() {
+  const dateObj = new Date();
+  const currentTime = dateObj.toLocaleTimeString();
+  timeBlock.textContent = currentTime;
+  setTimeout(showTime, 1000);
+  setTimeout(showDate, 1000);
+}
+
+showTime();
+
+function showDate() {
+  const dateObj = new Date();
+  const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+  };
+  const currentDate = dateObj.toLocaleDateString('en-EN', options);
+  dateBlock.textContent = currentDate;
+}
+
+showDate();
+
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours();
+  const timeOfDay = ['night', 'morning', 'day', 'evening'];
+  const timeIndex = Math.floor(hours / 6);
+  greetingBlock.textContent = `Good ${timeOfDay[timeIndex] === 'day' ? 'afternoon' : timeOfDay[timeIndex]}`;
+
+  return timeOfDay[timeIndex];
+}
+
+getTimeOfDay();
+
+function setLocalStorage() {
+  localStorage.setItem('name', nameBlock.value);
+  localStorage.setItem('city', cityInput.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem('name')) {
+    nameBlock.value = localStorage.getItem('name');
+  }
+  if (localStorage.getItem('city')) {
+    cityInput.value = localStorage.getItem('city');
+    getWeather();
+  } else {
+    cityInput.value = 'Minsk';
+    getWeather();
+  }
+}
+window.addEventListener('load', getLocalStorage);
+
+function setBg() {
+  const bgNum = randomNum;
+  const bgString = bgNum.toString().padStart(2, '0');
+
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${getTimeOfDay()}/${bgString}.jpg`;
+
+  img.onload = () => {
+    body.style.backgroundImage = `url('${img.src}')`;
+  };
+}
+
+setBg();
+
+function getSlideNext() {
+  if (randomNum >= 20) {
+    randomNum = 1;
+  } else {
+    randomNum++;
+  }
+
+  setBg();
+}
+
+slideNext.addEventListener('click', getSlideNext);
+
+function getSlidePrev() {
+  if (randomNum === 1) {
+    randomNum = 20;
+  } else {
+    randomNum--;
+  }
+
+  setBg();
+}
+
+slidePrev.addEventListener('click', getSlidePrev);
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${Math.round(data.main.temp)}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+  wind.textContent = `Wind speed: ${data.wind.speed} m/s`;
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+}
+
+cityInput.addEventListener('change', getWeather);
+
+async function getQuote() {
+  const url = 'https://type.fit/api/quotes';
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const random = Math.floor(Math.random() * data.length);
+
+  quote.textContent = data[random].text;
+  quoteAuthor.textContent = data[random].author;
+}
+
+getQuote();
+
+quoteBtn.addEventListener('click', getQuote);
+
+function playAudio() {
+  if (!isPlay) {
+    isPlay = true;
+    audio.src = playList[playNum].src;
+    audio.currentTime = 0;
+    audio.play();
+    highlightActiveTrack();
+  } else {
+    isPlay = false;
+    audio.pause();
+    highlightActiveTrack();
+  }
+}
+
+function toggleBtn() {
+  if (isPlay) {
+    playBtn.classList.add('pause');
+  } else {
+    playBtn.classList.remove('pause');
+  }
+}
+
+playBtn.addEventListener('click', playAudio);
+playBtn.addEventListener('click', toggleBtn);
+
+function playNext() {
+  if (playNum >= playList.length - 1) {
+    playNum = 0;
+  } else {
+    playNum++;
+  }
+  isPlay = false;
+  playAudio();
+  toggleBtn();
+}
+
+function playPrev() {
+  if (playNum <= 0) {
+    playNum = playList.length - 1;
+  } else {
+    playNum--;
+  }
+  isPlay = false;
+  playAudio();
+  toggleBtn();
+}
+
+nextBtn.addEventListener('click', playNext);
+prevBtn.addEventListener('click', playPrev);
+
+function createTrack(trackName) {
+  const li = document.createElement('li');
+  li.classList.add('play-item');
+  li.textContent = trackName;
+  playListContainer.append(li);
+}
+
+playList.forEach(el => createTrack(el.title));
+
+function highlightActiveTrack() {
+  const list = document.querySelectorAll('li.play-item');
+  list.forEach(el => el.classList.remove('item-active'));
+  list[playNum].classList.add('item-active');
+}
+
+audio.addEventListener('timeupdate', loopPlaylist);
+
+function loopPlaylist() {
+  if (audio.currentTime >= audio.duration) {
+    playNext();
+  }
+}
+
+audio.addEventListener('timeupdate', changeAudioProgress);
+
+function changeAudioProgress() {
+  const percentage = (audio.currentTime / audio.duration) * 100;
+  audioProgress.style.background = `linear-gradient(to right, #dcc659 0%, #dcc659 ${percentage}%, #fff ${percentage}%, #fff 100%)`;
+}
+
+audioProgress.addEventListener('click', rewindAudio);
+audioProgress.addEventListener('mousemove', (e) => isMouseDown && rewindAudio(e));
+audioProgress.addEventListener('mousedown', () => isMouseDown = true);
+audioProgress.addEventListener('mouseup', () => isMouseDown = false);
+
+function rewindAudio(e) {
+  const time = (e.offsetX / audioProgress.offsetWidth) * audio.duration;
+  audio.currentTime = time;
+}
+
+audioVol.addEventListener('click', changeAudioVolume);
+audioVol.addEventListener('mousemove', (e) => isMouseDown && changeAudioVolume(e));
+audioVol.addEventListener('mousedown', () => isMouseDown = true);
+audioVol.addEventListener('mouseup', () => isMouseDown = false);
+
+function changeAudioVolume() {
+  audio.volume = audioVol.value;
+  audio.muted = false;
+  if (audio.volume == 0) {
+    volBtn.classList.remove('fa-volume-up');
+    volBtn.classList.add('fa-volume-mute');
+  } else {
+    volBtn.classList.remove('fa-volume-mute');
+    volBtn.classList.add('fa-volume-up');
+  }
+}
+
+changeAudioVolume();
+
+audio.addEventListener('timeupdate', showTimeProgress);
+
+function showTimeProgress() {
+  const time = document.querySelector('.progress span');
+  const durMin = Math.floor(audio.duration / 60);
+  const durSec = Math.round(audio.duration - (durMin * 60)).toString().padStart(2, '0');
+
+  const curMin = Math.floor(audio.currentTime / 60);
+  const curSec = Math.round(audio.currentTime - (curMin * 60)).toString().padStart(2, '0');
+
+  if (!isNaN(durMin)) {
+    time.textContent = `${curMin}:${curSec} / ${durMin}:${durSec}`;
+  }
+}
+
+volBtn.addEventListener('click', muteAudio);
+
+function muteAudio() {
+  if (audio.muted) {
+    audio.muted = false;
+    volBtn.classList.remove('fa-volume-mute');
+    volBtn.classList.add('fa-volume-up');
+  } else {
+    audio.muted = true;
+    volBtn.classList.remove('fa-volume-up');
+    volBtn.classList.add('fa-volume-mute');
+  }
+}
